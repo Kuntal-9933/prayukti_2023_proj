ce2d441cfddb5c41900979263a896863fd4a5e0e
Kuntal <kuntalmukherjee702@gmail.com>
2023-03-20 10:17:10 +0530

    new file added

---
+++ frontend_codes/test_connection_files.py
@@ -0,0 +1,9 @@
+import os
+import time
+start = time.time()
+end = time.time()
+if (end-start)<10:
+    os.system("python F:\\jarvis_code_files\\prayukti_2023_proj\\frontend_codes\\test2.py")
+else:
+    print(-1)
+    
