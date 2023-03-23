bacae4721f6b7c4d3c3a0229f1d2b7c07c7354e5
Syed Saif Ahmed <syedsaifahmed55@gmail.com>
2023-03-24 01:07:08 +0530

    Test2.py updated

--- frontend_codes/test2.py
+++ frontend_codes/test2.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QMovie
+import time
 
 
 class Ui_MainWindow(object):
@@ -12,14 +13,14 @@ class Ui_MainWindow(object):
         self.label.setGeometry(QtCore.QRect(0, 0, 891, 501))
         self.label.setAutoFillBackground(True)
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("F:\\jarvis_code_files\\prayukti_2023_proj\\frontend_codes\\B.G\\Black_Template.jpg"))
+        self.label.setPixmap(QtGui.QPixmap(r"F:\VS_code\jarvis\prayukti_2023_proj\frontend_codes\B.G\Black_Template.jpg"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(1, 1, 891, 501))
         self.label_2.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.label_2.setText("")
-        self.movie = QMovie("F:\\jarvis_code_files\\prayukti_2023_proj\\frontend_codes\\ExtraGui\\WELCOME_PAGE.gif")
+        self.movie = QMovie("F:\\VS_code\\jarvis\\prayukti_2023_proj\\frontend_codes\\ExtraGui\\WELCOME_PAGE.gif")
         self.label_2.setMovie(self.movie)
         self.label_2.setScaledContents(True)
         self.label_2.setObjectName("label_2")
@@ -35,6 +36,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+    
 
 
 if __name__ == "__main__":
@@ -44,4 +46,6 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    QtCore.QTimer.singleShot(5100,MainWindow.close)
     sys.exit(app.exec_())
+
